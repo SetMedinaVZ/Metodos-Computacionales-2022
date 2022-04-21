a72aee1ff7b80ab08b9586489ddf66c5a705e5df
Sebastian <sebastian.medina252@gmail.com>
2022-04-20 20:00:17 -0500

    isReal Listo

--- Act 3.2.py
+++ Act 3.2.py
@@ -1,8 +1,32 @@
 import re
 
+def limpiaLista(array):
+    aux = []
+    for i in array:
+        for j in i:
+            if j != '':
+                aux.append(j)
+                
+    return aux
+
+def imprimeResultado(lista, tipo):
+    for i in lista:
+            print(i +'\t' +tipo)
+                   
+
+def isReal(string):
+    x = re.findall("([+-]?[0-9]+\.[0-9]+[eE][+-]?[0-9]{1,3})|([+-]?\.[0-9]+[eE][+-]?[0-9]{1,3})|([+-]?[0-9]+\.?[eE][+-]?[0-9]{1,3})|([+-]?[0-9]+\.[0-9]+)|([+-]?\.[0-9]+)|([+-]?[0-9]+\.?)", string)
+    if not x:
+        return False
+    else:
+        lista = limpiaLista(x)
+        imprimeResultado(lista, 'Real')  
+
 def lexerAritmetico(strings):
     print("\nToken\tTipo")
-    
+    for i in strings:
+        isReal(i)
+
 
 def main():
     strings = []
@@ -10,8 +34,6 @@ def main():
         lineas = f.readlines()
         for linea in lineas:
             strings.append(linea.strip('\n'))
-    print (strings)
-
     lexerAritmetico(strings)
 
 main()
