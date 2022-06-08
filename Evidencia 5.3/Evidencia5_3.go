7bb8f9ddb16a18812920dbbcff29e1c91ce514e2
Sebastian <sebastian.medina252@gmail.com>
2022-06-08 18:41:01 -0500

    Intento

--- Evidencia 5.3/Evidencia5_3.go
+++ Evidencia 5.3/Evidencia5_3.go
@@ -6,94 +6,22 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"strings"
 )
 
-//Tokens
-const (
-	//Palabras reservadas
-	KW = 501
-	//Variables
-	ID = 502
-	//Numeros
-	INT = 503
-	FLOAT = 504
-	//Comentarios
-	COMM = 505
-	//Operadores aritmeticos
-	OPA = 506
-	//Asignacion
-	ASI = 507
-	//Operadores logicos
-	OPL = 508
-	//Delimitadores
-	DEL = 509
-	//Error
-	ERR = 510
-	//Blanco
-	BLA = 511
-	//EOF
-	EOF = 512
-)
-
-//Objeto que contiene el lexema y el token
-type tokens struct {
-	lexema string
-	token int
+//Palabras reservadas
+var reservadas = []string{
+	"True", "False", "None", "and", "as", "assert", 
+	"break", "class", "continue", "def", "del", "elif", 
+	"else", "except", "finally", "for", "from", "global", 
+	"if", "import", "in", "is", "lambda", "nonlocal", "not", "or", 
+	"pass", "raise", "return", "try", "while", "with", "yield",
 }
 
-func esReservada(lexema string) bool {
-	reservadas := []string{"True", "False", "None", "and", "as", "assert", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try", "while", "with", "yield"}
-	for _, r := range reservadas {
-		if lexema == r {
-			return true
-		}
-	}
-	return false
+//Operadores aritmeticos
+var operadoresAritmeticos = []string{
+	"+", "-", "*", "/", "%", "**",
 }
 
-func esEntero(lexema string) bool {
-	for _, r := range lexema {
-		if r < '0' || r > '9' {
-			return false
-		}
-	}
-	return true
-}
-
-func esFloat(lexema string) bool {
-	var contador int
-	for _, r := range lexema {
-		if r == '.' {
-			contador++
-		}
-		if r < '0' || r > '9' {
-			return false
-		}
-	}
-	if contador > 1 {
-		return false
-	}
-	return true
-} 
-
-func scanner() {
-	//Variables para el analisis
-	lexema := "" //Palabra que genera el token
-	tokens := []tokens{} //Lista de tokens
-	leer := true //Bandera para leer caracter
-	
-	//Leer el archivo
-	archivo, err := bufio.NewReader(strings.NewReader(texto)).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	char := string(archivo[0]) //Caracter inicial
-
-	//Analisis lexico
-	for leer {
-		
-}
